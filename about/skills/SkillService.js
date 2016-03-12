9ca69c194a328d1633f800dd0f9db8fe133aee55
AnatolyKovalenko <mr.anatolykovalenko@gmail.com>
2016-03-12 09:40:49 +0200

    Added skills template

---
+++ about/skills/SkillService.js
@@ -0,0 +1,16 @@
+app
+	.factory('SkillService',SkillService);
+
+	function SkillService($http){
+		var skills = {};
+
+		skills.getInfoSkills = function(){
+	 		return $http
+				.get('api/skills.json')
+				.then(function(response){ 
+					return response;
+				});
+		};
+		return skills;
+	}
+	
