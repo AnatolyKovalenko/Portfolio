1ae26e677aa139677a697daa1ca5ac597d027c42
AnatolyKovalenko <mr.anatolykovalenko@gmail.com>
2016-03-14 16:43:48 +0200

    Fixed little bug with tabs

--- about/skills/SkillService.js
+++ about/skills/SkillService.js
@@ -1,16 +1,15 @@
 app
 	.factory('SkillService',SkillService);
 
-	function SkillService($http){
+	function SkillService($http) {
 		var skills = {};
 
-		skills.getInfoSkills = function(){
-	 		return $http
+		skills.getInfoSkills = function() {
+			return $http
 				.get('api/skills.json')
-				.then(function(response){ 
+				.then(function(response) {
 					return response;
 				});
 		};
 		return skills;
 	}
-	
--- projects/ProjectsCtrl.js
+++ projects/ProjectsCtrl.js
@@ -4,7 +4,7 @@
 	app
 		.controller('ProjectsCtrl', projectsCtrl);
 
-	function projectsCtrl(){
+	function projectsCtrl() {
 		var phone = document.getElementById("phone_1"),
 				iframe = document.getElementById("frame_1"),
 				phones = document.getElementById("phones"),
@@ -13,40 +13,40 @@
 				phone_height;
 
 		function updateView(view) {
-		  if (view) {
-		    phone.className = "phone view_" + view;
-		  }
+			if (view) {
+				phone.className = "phone view_" + view;
+			}
 		}
 
 		views.addEventListener("click", function(evt) {
-		  updateView(evt.target.value);
+			updateView(evt.target.value);
 		});
 
 		phones.addEventListener("click", function(evt) {			
 			switch(parseInt(evt.target.value)){
 				case 1:
-		      phone_width = 375;
-		      phone_height = 667;
+					phone_width = 375;
+					phone_height = 667;
 				break;
 
 				case 2:
-		      phone_width = 400;
-		    	phone_height = 640;
+					phone_width = 400;
+					phone_height = 640;
 				break; 
 				
 				case 3:
-		     	phone_width = 320;
-		    	phone_height = 480;
+					phone_width = 320;
+					phone_height = 480;
 				break;
 				
 				case 4:
-		     	phone_width = 360;
-		    	phone_height = 640; 
+					phone_width = 360;
+					phone_height = 640; 
 				break;
 
 				case 5:
-		     	phone_width = 768;
-		    	phone_height = 1024;
+					phone_width = 768;
+					phone_height = 1024;
 				break;
 			}
 
@@ -56,22 +56,22 @@
 		});
 
 		if (iframe.attachEvent){
-		  iframe.attachEvent("onload", function(){
-		    afterLoading();
-		  });
+			iframe.attachEvent("onload", function() {
+				afterLoading();
+			});
 		} else {
-		  iframe.onload = function() {
-		    afterLoading();
-		  };
+			iframe.onload = function() {
+				afterLoading();
+			};
 		}
 
 		function afterLoading() {
-		  setTimeout(function() {
-		    phone.className = "phone view_1";
-		    setTimeout(function() {
-		      phone.className = "phone view_1 rotate";
-		    }, 1000);
-		  }, 1000);
+			setTimeout(function() {
+				phone.className = "phone view_1";
+				setTimeout(function() {
+					phone.className = "phone view_1 rotate";
+				}, 1000);
+			}, 1000);
 		}
 
 	}
