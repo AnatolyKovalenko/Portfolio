a911d34ce8a92351bd6cd0cb5490887d00cd57b1
AnatolyKovalenko <mr.anatolykovalenko@gmail.com>
2016-03-15 11:12:56 +0200

    Change tabs

--- router.js
+++ router.js
@@ -1,28 +1,28 @@
 app
-	.config(function($stateProvider, $urlRouterProvider) {
+  .config(function($stateProvider, $urlRouterProvider) {
 
-	$urlRouterProvider	
-		.otherwise("/about");
+  $urlRouterProvider  
+    .otherwise("/about");
 
-	$stateProvider
-			.state('about', {
-				url: "/about",
-				templateUrl: "about/AboutView.html",
-				controller: 'AboutCtrl'
-			})
-			.state('about.skills', {
-				url: "/skills",
-				templateUrl: "about/skills/SkillsView.html",
-				controller: 'SkillsCtrl'
-			})
-			.state('projects', {
-				url: "/projects",
-				templateUrl: "projects/ProjectsView.html",
-				controller: "ProjectsCtrl"
-			})
-			.state('connect', {
-				url: "/connect",
-				templateUrl: "connect/ConnectView.html",
-				controller: "ConnectCtrl"
-			});
-	});
+  $stateProvider
+      .state('about', {
+        url: "/about",
+        templateUrl: "about/AboutView.html",
+        controller: 'AboutCtrl'
+      })
+      .state('about.skills', {
+        url: "/skills",
+        templateUrl: "about/skills/SkillsView.html",
+        controller: 'SkillsCtrl'
+      })
+      .state('projects', {
+        url: "/projects",
+        templateUrl: "projects/ProjectsView.html",
+        controller: "ProjectsCtrl"
+      })
+      .state('connect', {
+        url: "/connect",
+        templateUrl: "connect/ConnectView.html",
+        controller: "ConnectCtrl"
+      });
+  });
